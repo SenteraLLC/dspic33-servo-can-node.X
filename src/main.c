e7fcbacc03c6e8e8ce6b75dbf8a44a729f253b88
BrianEickhoff <brian.eickhoff@sentera.com>
2015-05-03 23:09:54 -0500

    Added INA219 task to the main program loop.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <xc.h>
 #include "cfgbits.h"
 #include "init.h"
+#include "ina219.h"
 
 
 //==============================================================================
@@ -11,10 +12,10 @@ int main()
 {
     InitBoard();        // Initialize processor & peripherals.
     
-    
     for (;;)
     {
-        asm("CLRWDT");      // Clear the watchdog timer.
+        asm("CLRWDT");              // Clear the watchdog timer.
+        INA219Task();               // Current and power monitor.
     }
     
     return 0;
